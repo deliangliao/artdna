82fe7a82149df879ee0a06fbc6ce033162f16670
廖德良 <deliangliao@gmail.com>
2016-03-21 10:38:28 +0800

    1.测试banner数量 到4张 2.首页9宫格链接到h5

--- app/src/main/java/com/artdna/ui/activity/HomeActivity.java
+++ app/src/main/java/com/artdna/ui/activity/HomeActivity.java
@@ -1,16 +1,19 @@
 package com.artdna.ui.activity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.annotation.Nullable;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import com.artdna.R;
+import com.artdna.config.ArtKey;
 import com.artdna.ui.base.BaseArtActivity;
 import com.artdna.utils.TopUtil;
 import com.jude.rollviewpager.RollPagerView;
 import com.jude.rollviewpager.adapter.LoopPagerAdapter;
+import com.jude.rollviewpager.hintview.ColorPointHintView;
 
 import butterknife.Bind;
 import butterknife.OnClick;
@@ -41,14 +44,17 @@ public class HomeActivity extends BaseArtActivity {
 
     @Override
     protected void initData() {
-//        mRollViewPager.setHintView(new ColorPointHintView(mContext, Color.GRAY, Color.WHITE));
-        mRollViewPager.setHintView(null);//隐藏指示器
+        mRollViewPager.setHintView(new ColorPointHintView(mContext, getColorById(R.color.colorPrimary), Color.WHITE));
+//        mRollViewPager.setHintView(null);//隐藏指示器
         mRollViewPager.setAdapter(new TestLoopAdapter(mRollViewPager));
     }
 
     private class TestLoopAdapter extends LoopPagerAdapter {
         private int[] imgs = {
-                R.drawable.dna_banner,
+                R.drawable.home_banner_1,
+                R.drawable.home_banner_2,
+                R.drawable.home_banner_3,
+                R.drawable.home_banner_4
         };
 
         public TestLoopAdapter(RollPagerView viewPager) {
@@ -76,9 +82,55 @@ public class HomeActivity extends BaseArtActivity {
         return "艺管家";
     }
 
-    @OnClick({R.id.top_right, R.id.ic_dsjgl, R.id.ic_xxjy, R.id.ic_xsjy, R.id.ic_pm, R.id.ic_zydk, R.id.ic_jrfw, R.id.ic_jdpg, R.id.ic_pxjl})
+    @OnClick(R.id.top_right)
     public void toastMsg() {
         toast("让艺术品都流通起来");
+    }
+
+    @OnClick(R.id.ic_dsjgl)
+    public void dodsjgl() {
+        toWebActivity("http://a.eqxiu.com/s/k7wpGNwI");
+    }
+
+    @OnClick(R.id.ic_xsjy)
+    public void doxsjy() {
+        toWebActivity("http://e.eqxiu.com/s/nhYjIM4R");
+    }
+
+    @OnClick(R.id.ic_xxjy)
+    public void doxxjy() {
+        toWebActivity("http://a.eqxiu.com/s/boAUjzay");
+    }
+
+    @OnClick(R.id.ic_pm)
+    public void dopm() {
+        toWebActivity("http://g.eqxiu.com/s/N97Ua5sF");
+    }
+
+    @OnClick(R.id.ic_zydk)
+    public void dozydk() {
+        toWebActivity("http://q.eqxiu.com/s/fdXJo2hj");
+    }
+
+    @OnClick(R.id.ic_jrfw)
+    public void dojrfw() {
+        toWebActivity("http://b.eqxiu.com/s/upwFebEW");
+    }
+
+    @OnClick(R.id.ic_jdpg)
+    public void dojdpg() {
+        toWebActivity("http://g.eqxiu.com/s/EslsqEct");
+    }
+
+    @OnClick(R.id.ic_pxjl)
+    public void dopxjl() {
+        toWebActivity("http://q.eqxiu.com/s/e45x1Sx0");
+    }
+
+    public void toWebActivity(String url) {
+        Intent intent = new Intent(mContext, WebActivity.class);
+        intent.putExtra(ArtKey.KEY_INTENT_WEB_URL, url);
+        startActivity(intent);
     }
 
 }
